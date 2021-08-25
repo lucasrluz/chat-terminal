3b57cd34bc82ccbb43259eb3f55ec1bd1d301249
lucasrluz <lucasr.luzbr@gmail.com>
2021-08-25 16:47:42 -0300

    Habilitando as rotas e o uso de JSON

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
+import { router } from './route';
 
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
+
+app.use(express.json());
+app.use(router);
 
 httpServer.listen(3000);
