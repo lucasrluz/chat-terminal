bba6afebb13b396a682196723dcf24189499e801
lucasrluz <lucasr.luzbr@gmail.com>
2021-08-25 16:39:34 -0300

    Chamada da função que renderiza as opções iniciais

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client';
+import { renderOptions } from './renderOptions';
 
 async function start() {
+  const username: string = await renderOptions();
+
   const socket = io('http://localhost:3000');
 }
 start();
