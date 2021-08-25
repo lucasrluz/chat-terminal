2b6a8f7ffa1df385c6b24f7f4d25fd74950cb084
lucasrluz <lucasr.luzbr@gmail.com>
2021-08-25 14:45:47 -0300

    Fluxo quando opção for logar no sistema

--- client/src/renderOptions.ts
+++ client/src/renderOptions.ts
@@ -1,3 +1,6 @@
+import { login } from "./login";
+import { question } from "./readline";
+
 export async function renderOptions() {
   while(true) {
     process.stdout.write('\u001b[H\u001b[2J\u001b[3J');
@@ -5,5 +8,12 @@ export async function renderOptions() {
     console.info('[ 0 ] - Login');
     console.info('[ 1 ] - New Account');
     console.info('[ 2 ] - Close Application');
+
+    const option: any = await question('> ');
+    
+    if (option === '0') {
+      var username: string = await login();
+      break;
+    }
   }
 }
